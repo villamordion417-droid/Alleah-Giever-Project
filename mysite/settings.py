1036c0b2f6887bbd17c8735e958363e6c50e96e1
Dion <your-github-villamordion417@gmail.com>
2026-03-25 20:23:44 +0900

    first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
-    'whitenoise.runserver_nostatic'
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
